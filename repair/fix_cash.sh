0c7e474200f43b236910372392f8373ea3045bd7
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-14 05:03:43 +0800

    Update fix_cash.sh

--- repair/fix_cash.sh
+++ repair/fix_cash.sh
@@ -1,6 +1,6 @@
 tCnt=`cat .showcnt.txt`
 for ((i=1; i<=tCnt; i ++))
 do
-sed -i 's#* * * /root/cashout#* * * root /root/cashout#g' /etc/crontab
+sed -i 's#/root/cashout#root /root/cashout#g' /etc/crontab
 done
 echo "已修复root用户的自动兑现问题。"

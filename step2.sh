f291498d901fa95962aca5505edd019c6c6ae37f
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-14 02:23:52 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -13,7 +13,7 @@ do
 echo "对第$i个节点添加自动提取。"
 cp /var/lib/bee/node${i}/keys/swarm.key ./keys
 mv ./keys/swarm.key ./keys/${ip}-${i}.key
-echo "00 02 * * * /root/cashout${i}.sh cashout-all" >> /etc/crontab
+echo "00 02 * * * root /root/cashout${i}.sh cashout-all" >> /etc/crontab
 screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'

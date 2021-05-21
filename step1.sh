e2cba6e0388cb3c101e671ffe25ea8d143403458
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-21 19:04:50 +0800

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -8,11 +8,11 @@ sudo apt-get install -y jq
 sudo apt-get install -y lrzsz
 sudo apt-get install -y screen
 sudo apt-get install -y net-tools
-wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.deb
+wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.deb
 wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/b40510f1172b96c21d6d20558ca1e70d26d625c4/cashout.sh && chmod 777 cashout.sh
 wget https://raw.githubusercontent.com/pumpkin4gb/bzz/main/step2.sh && chmod 777 step2.sh
 wget https://raw.githubusercontent.com/pumpkin4gb/bzz/main/step3.sh && chmod 777 step3.sh
-sudo dpkg -i bee_0.5.3_amd64.deb && sudo chown -R bee:bee /var/lib/bee
+sudo dpkg -i bee_0.6.0_amd64.deb && sudo chown -R bee:bee /var/lib/bee
 echo "0" > $cntFile
 chmod +rw $cntFile
 sed -i 's/10000000000000000/1/g' cashout.sh
@@ -33,16 +33,18 @@ echo "    这是第 $tCnt 次创建节点"
 echo "    若需更改endpoint，请自行修改epFile.txt"
 cat>node${tCnt}.yaml<<EOF
 api-addr: :$((1534+${tCnt}))
-config: /root/node${tCnt}.yaml
+#config: /root/node${tCnt}.yaml
 data-dir: /var/lib/bee/node${tCnt}
 db-capacity: 15000000
-#debug-api-addr: :$((1634+${tCnt}))
-debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
+debug-api-addr: :$((1634+${tCnt}))
+#debug-api-addr: 127.0.0.1:$((1634+${tCnt}))
 debug-api-enable: true
 p2p-addr: :$((1734+${tCnt}))
 password-file: /var/lib/bee/password
+swap-initial-deposit: "10000000000000000"
 verbosity: 5
 swap-endpoint: ${ep}
+full-node: true
 EOF
 cp cashout.sh cashout${tCnt}.sh
 sed -i "s/1635/$((1634+${tCnt}))/g" cashout${tCnt}.sh

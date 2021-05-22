99a13efa6725309ae35cb57074fe45bf3d825103
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-05-22 18:07:08 +0800

    Update update_bee.sh

--- update_bee.sh
+++ update_bee.sh
@@ -32,7 +32,7 @@ full-node: true
 EOF
 done
 wget https://github.com/ethersphere/bee/releases/download/v0.6.1/bee_0.6.1_amd64.deb
-sudo dpkg -i bee_0.6.0_amd64.deb
+sudo dpkg -i bee_0.6.1_amd64.deb
 for ((i=1; i<=tCnt; i ++))
 do
 screen -dmS bee$i

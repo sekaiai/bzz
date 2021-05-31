3696b8d1361dbed199a6a0b4d255085376e166f4
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-06-01 00:05:18 +0800

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -1,8 +1,11 @@
 tCnt=`cat .showcnt.txt`
 ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 echo "ip,num,dpi_port,address,chequebook" > ${ip}.txt
+mkdir -p keys
 for ((i=1; i<=tCnt; i ++))
 do
+cp /var/lib/bee/node${i}/keys/swarm.key ./keys
+mv ./keys/swarm.key ./keys/${ip}-${i}.key
 dpi_port=`cat node${i}.yaml | grep 'debug-api-addr: 127.0.0.1:' | awk -F ':' '{print $3}'`
 echo "节点${i}的端口为：${dpi_port}"
 echo "节点${i}的钱包地址和合约地址:"

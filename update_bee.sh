95a17595b5a2dd1b05fbca14d8a35e1691fc87ca
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-06-09 18:20:29 +0800

    Update update_bee.sh

--- update_bee.sh
+++ update_bee.sh
@@ -53,6 +53,7 @@ screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已启动。"
+sleep 2
 done
 screen -wipe
 screen -ls

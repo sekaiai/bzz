b811e9e96473331843d5ff7b695f73c39f9045d8
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-06-09 18:19:29 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -15,5 +15,6 @@ screen -dmS bee$i
 screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已启动。"
-screen -ls
+sleep 2
 done
+screen -ls

82b2f8417669e571830b5e137b1aabe084de4195
pumpkin4gb <53449355+pumpkin4gb@users.noreply.github.com>
2021-06-10 13:04:31 +0800

    新增：5分钟一次监听bee端口，若挂则重启bee

--- step2.sh
+++ step2.sh
@@ -18,3 +18,27 @@ echo "第$i个节点已启动。"
 sleep 2
 done
 screen -ls
+echo "尝试添加监听脚本……"
+[[ -f /root/bee_watcher.sh ]] && echo "已存在监听脚本！" && exit 1
+echo "5 * * * * root /root/bee_watcher.sh" >> /etc/crontab
+cat > /root/bee_watcher.sh <<'EOF'
+#!/usr/bin/env bash
+cntFile=".showcnt.txt"
+if [ ! -f $cntFile ]; then
+echo "未运行step1！"
+exit
+fi
+tCnt=`cat $cntFile`
+for ((i=1; i<=tCnt; i ++))
+do
+lsof -i:$((1534+$i))
+if [ $? -ne 0 ]
+then
+screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
+screen -x -S bee$i -p 0 -X stuff $'\n'
+echo "第$i个节点已启动。"
+fi
+done
+EOF
+echo "已创建监听脚本。"
+chmod 777 /root/bee_watcher.sh

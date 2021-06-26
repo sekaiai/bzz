f031bc0c8b258050e2ecd8b8e07645fd2f54636a
dislikediss <36874994+dislikediss@users.noreply.github.com>
2021-06-27 00:57:06 +0800

    自启脚本

---
+++ beev3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "ming.cash 微信 dhaidao bee 掉线假死重启脚本 ";
+i=0
+while true
+do
+    res=`ps -ef | grep "./$1 $3" | grep -v grep | wc -l`
+    if [ $res -eq 0 ]
+    then
+        i=0
+	echo "启动命令 ./$1 $3";
+	./$1 $3
+    fi    
+    result_code=`curl -I -m 10 -o /dev/null -s -w %{http_code} http://127.0.0.1:$2/health`
+    if [ ${result_code} -ne 200 ]
+    then
+        i=$(($i+1))
+        echo "掉线了 重启计数 ${i}  i=12重启";
+		if [ ${i} -eq 12 ]
+		then
+			`ps -efww|grep './work101 .*1639' |grep -v grep|cut -c 9-15|xargs kill -9`
+			./$1 $3
+			i=0
+		fi	
+    fi
+    sleep 5s
+done

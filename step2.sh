a498412fb2987609a91e26b4f9bb2e666b3b19b6
dislikediss <36874994+dislikediss@users.noreply.github.com>
2021-06-27 00:54:54 +0800

    添加掉线重启脚本

--- step2.sh
+++ step2.sh
@@ -12,34 +12,9 @@ do
 echo "对第$i个节点添加自动提取。"
 echo "00 02 * * * root /root/cashout${i}.sh cashout-all" >> /etc/crontab
 screen -dmS bee$i
-screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
+echo "beev3为无产阶级社区&MingCash贡献的掉线重启脚本，您正启动第$i个节点"
+screen -x -S bee$i -p 0 -X stuff "./beev3.sh bee $((1634+${i})) 'start --config node${i}.yaml"
 screen -x -S bee$i -p 0 -X stuff $'\n'
 echo "第$i个节点已启动。"
 sleep 2
 done
-screen -ls
-echo "尝试添加监听脚本……"
-[[ -f /root/bee_watcher.sh ]] && echo "已存在监听脚本！" && exit 1
-echo "*/5 * * * * root /root/bee_watcher.sh" >> /etc/crontab
-cat > /root/bee_watcher.sh <<'EOF'
-#!/usr/bin/env bash
-cntFile=".showcnt.txt"
-if [ ! -f $cntFile ]; then
-echo "未运行step1！"
-exit
-fi
-tCnt=`cat $cntFile`
-for ((i=1; i<=tCnt; i ++))
-do
-lsof -i:$((1534+$i))
-if [ $? -ne 0 ]
-then
-screen -x -S bee$i -p 0 -X stuff "bee start --config node${i}.yaml"
-screen -x -S bee$i -p 0 -X stuff $'\n'
-echo "第$i个节点已启动。"
-sleep 2
-fi
-done
-EOF
-echo "已创建监听脚本。"
-chmod 777 /root/bee_watcher.sh
